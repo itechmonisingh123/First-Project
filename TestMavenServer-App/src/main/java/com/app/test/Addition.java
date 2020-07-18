c7ff530cc58fc086071cf1e6a9fdf9dabc92a619
itechmonisingh123 <68458918+itechmonisingh123@users.noreply.github.com>
2020-07-18 14:54:37 +0530

    Update Addition.java
    added sub Method

--- TestMavenServer-App/src/main/java/com/app/test/Addition.java
+++ TestMavenServer-App/src/main/java/com/app/test/Addition.java
@@ -11,6 +11,9 @@ public class Addition {
 	public int sum( int a, int b) {
 		return a+b;
 	}
+	public int sub(int a, int b) {
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		Addition ab=new Addition();
