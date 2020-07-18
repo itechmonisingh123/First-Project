db85d1a1ffcb7acb7410ab20257420ad8eaf4417
moni singh <moni singh@Moni>
2020-07-18 14:50:40 +0530

    23456-Addition Program-added one method

---
+++ TestMavenServer-App/src/main/java/com/app/test/Addition.java
@@ -0,0 +1,19 @@
+package com.app.test;
+
+public class Addition {
+	
+	private int a;
+	private int b;
+	
+	public Addition() {
+		
+	}
+	public int sum( int a, int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		Addition ab=new Addition();
+		System.out.println(ab.sum(5,6));
+	}
+}
